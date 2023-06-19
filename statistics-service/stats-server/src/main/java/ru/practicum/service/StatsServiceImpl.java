30eb2cb5237aa4e2e2aabef5f6f8e495b6909e95
LeonovichMariia <MaryKrylova17@yandex.ru>
2023-06-20 02:23:21 +0300

    fix: code style

--- statistics-service/stats-server/src/main/java/ru/practicum/service/StatsServiceImpl.java
+++ statistics-service/stats-server/src/main/java/ru/practicum/service/StatsServiceImpl.java
@@ -3,7 +3,6 @@ package ru.practicum.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.exception.BadRequestException;
